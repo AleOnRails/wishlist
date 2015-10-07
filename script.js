ac35b8f666d330269244750f8ef1769764d7b3e0
Alessandra Cannatà <pan.kairos@gmail.com>
2015-10-07 17:49:39 +0200

    added li + pending button

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /* Exercise 1: Wish list */
+
+function addToList(item) {
+  $("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span></li>");
+}
+
+$("document").ready(function() {
+
+  $("#add-to-list").click(function() {
+    addToList( $("#item").val() );
+    $("#item").val("");
+    $("#item").focus();
+  });
+});
+
