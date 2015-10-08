fad11f2472b2c09ed8e15fb01423319f64c6b475
Alessandra Cannatà <pan.kairos@gmail.com>
2015-10-08 11:23:32 +0200

    click + total count

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 
 function addToList(item) {
   $("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span></li>");
+
+  $(".pending").on("click", function(){
+    var p = $(this).parent();
+    p.addClass("completed");
+    $(this).remove();
+    p.append("<span class='label success'>Done!</span>");
+
+    updateTotal();
+  });
+
+  if($("li").length >= 1)
+    updateTotal();
 }
+
+
+function updateTotal() {
+  var pending = $(".pending").length;
+  var completed = $(".completed").length;
+  $(".total").text("Pending: " + pending + " Completed: " + completed);
+}
+
 
 $("document").ready(function() {
 
@@ -11,5 +31,5 @@ $("document").ready(function() {
     $("#item").val("");
     $("#item").focus();
   });
-});
 
+});
